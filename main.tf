9caace00aea76013ffdc52d4e0a94cdb6719ddd7
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-07-15 15:02:01 +0100

    Update Terraform to read JSON from S3

--- main.tf
+++ main.tf
@@ -2,17 +2,26 @@ provider "aws" {
   region = var.aws_region
 }
 
+variable "payload_s3_key" {
+  description = "The S3 key of the payload JSON file"
+  type        = string
+}
+
+data "aws_s3_object" "payload" {
+  bucket = "thesamuraibucket"
+  key    = var.payload_s3_key
+}
+
 locals {
-  ec2_instances = jsondecode(file("${path.module}/payloads/sample.json")).instances
+  payload_content = jsondecode(data.aws_s3_object.payload.body)
 }
 
-module "ec2" {
-  source   = "./modules/ec2"
-  for_each = local.ec2_instances
+module "ec2_instances" {
+  source = "./modules/ec2"
+  for_each = local.payload_content.instances
 
-  name           = each.value.name
-  ami            = each.value.ami
-  instance_type  = each.value.instance_type
-  user_data      = each.value.user_data
+  name          = each.value.name
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
+  user_data     = each.value.user_data
 }
-
