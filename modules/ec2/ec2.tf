cd31d743c47f4c42867a14a35b27ba34e218aefa
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-07-18 15:33:54 +0100

    Remove duplicate public_ips output from ec2.tf, keep in outputs.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -4,14 +4,8 @@ resource "aws_instance" "this" {
   ami           = each.value.ami
   instance_type = each.value.instance_type
   user_data     = each.value.user_data
-
   tags = {
     Name = each.value.name
   }
-
   associate_public_ip_address = true
-}
-
-output "public_ips" {
-  value = { for k, v in aws_instance.this : k => v.public_ip }
 }
