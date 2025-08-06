02e7401b2a8ca32daa455597cfd5132a0cb5f36d
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-08-06 13:26:14 +0100

    Updated EKS with public subnets

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -14,8 +14,8 @@ resource "aws_eks_cluster" "cluster" {
 
   vpc_config {
   subnet_ids              = var.subnet_ids
-  endpoint_private_access = true
-  endpoint_public_access  = false
+  endpoint_private_access = false
+  endpoint_public_access  = true
 }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
