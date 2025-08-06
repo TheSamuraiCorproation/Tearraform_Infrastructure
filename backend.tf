1f568a08271ac77153f512e6fa0533baeadf1f8e
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-08-06 13:41:02 +0100

    Updated region to us-east-1 for backend and Jenkins

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "thesamuraibucket"
     key            = "terraform/state.tfstate"
-    region         = "eu-central-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
