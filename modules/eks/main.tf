e05bedc742c6c495540f5e747e8b956c0d87fcd0
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-08-08 14:00:22 +0100

    Enhance EKS with dynamic SG rules, update Jenkins validation, and review EC2/SG config

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -13,10 +13,10 @@ resource "aws_eks_cluster" "cluster" {
   version  = var.kubernetes_version
 
   vpc_config {
-  subnet_ids              = var.subnet_ids
-  endpoint_private_access = false
-  endpoint_public_access  = true
-}
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = false
+    endpoint_public_access  = true
+  }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
@@ -77,6 +77,61 @@ resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+# Data source to get the EKS cluster details
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.cluster.name
+}
+
+# Security group rule for node-to-control-plane communication (port 1025)
+resource "aws_security_group_rule" "node_to_control_plane_1025" {
+  type              = "ingress"
+  from_port         = 1025
+  to_port           = 1025
+  protocol          = "tcp"
+  security_group_id = "sg-08bb32aa5dd0cb5a0" # Update with your SG ID
+  source_security_group_id = data.aws_eks_cluster.cluster.vpc_config[0].security_group_ids[0]
+}
+
+# Security group rule for node-to-control-plane communication (ports 30000-32767)
+resource "aws_security_group_rule" "node_to_control_plane_30000_32767" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  security_group_id = "sg-08bb32aa5dd0cb5a0" # Update with your SG ID
+  source_security_group_id = data.aws_eks_cluster.cluster.vpc_config[0].security_group_ids[0]
+}
+
+# Security group rule for node-to-control-plane communication (port 443)
+resource "aws_security_group_rule" "node_to_control_plane_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = "sg-08bb32aa5dd0cb5a0" # Update with your SG ID
+  source_security_group_id = data.aws_eks_cluster.cluster.vpc_config[0].security_group_ids[0]
+}
+
+# Security group rule for node-to-node communication
+resource "aws_security_group_rule" "node_to_node" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  security_group_id = "sg-08bb32aa5dd0cb5a0" # Update with your SG ID
+  source_security_group_id = "sg-08bb32aa5dd0cb5a0"
+}
+
+# Ensure outbound rule allows all traffic
+resource "aws_security_group_rule" "outbound_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = "sg-08bb32aa5dd0cb5a0" # Update with your SG ID
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 output "cluster_endpoint" {
