a88d43c953c990ebd12a10571df2086c7d8ce9f2
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2026-02-25 13:13:49 +0100

    using the images of the ecr and integrating docker within the ec2 with ansible automating it

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -19,6 +19,12 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+# Allow EC2 instances to pull from ECR (needed so instance can run `aws ecr get-login-password`)
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role       = aws_iam_role.ec2_cloudwatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 resource "aws_iam_instance_profile" "ec2_cloudwatch_profile" {
   name = "EC2CloudWatchProfile-${random_id.unique_suffix.hex}"
   role = aws_iam_role.ec2_cloudwatch_role.name
