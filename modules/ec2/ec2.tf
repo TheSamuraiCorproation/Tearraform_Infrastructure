1bdb0677979ad71130b85cdcd26f2b1928d4bc2f
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2026-02-28 14:37:04 +0100

    set the ecÉ lab for larger disk size

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -40,7 +40,12 @@ resource "aws_instance" "this" {
   subnet_id              = var.subnet_id
   iam_instance_profile   = aws_iam_instance_profile.ec2_cloudwatch_profile.name
   associate_public_ip_address = true
-
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+  
   tags = merge(
     { Name = each.value.name },
     lookup(each.value, "tags", {})
