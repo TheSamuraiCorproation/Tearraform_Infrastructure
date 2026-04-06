30baa4b6c629534f685f3d55fd05da4050b81252
Mohamed Aziz Zaoueli <73081541+Shakoras@users.noreply.github.com>
2026-04-06 21:10:52 +0100

    feat: add exposure to the SG ID

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -5,3 +5,7 @@ output "public_ips" {
 output "ec2_instance_ids" {
   value = { for k, v in aws_instance.this : k => v.id }
 }
+
+output "vulnerable_vm_sg_id" {
+  value = aws_security_group.vulnerable_vm.id
+}
